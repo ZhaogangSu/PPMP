9b9f6a6736fc0c41458783502277b90a81ed1710
xiaozhou1 <xzhnfnu@163.com>
2025-10-17 15:10:15 +0800

    new test

--- scripts/precompile/precompile.jl
+++ scripts/precompile/precompile.jl
@@ -7,4 +7,9 @@ using ArgParse
 path = "data/grid/test_20_16/d0.3/f0.3/test_20_16_d0.3_f0.3_se1.json"
 
 julia_main(["OriginalMIPSolver", "0.05", path, ".", "--max-time", "35"])
-julia_main(["FlowCutSolver", "0.05", path, ".", "--max-time", "35"])
+julia_main(["FlowCutSolver", "0.1", path, ".",
+           "--mixing-inequality-type", "both",
+           "--merge-identical-scenarios", "false",
+           "--is-presolve-flowcut", "false",
+           "--mixing-print-level", "2",
+           "--max-time", "300"])
--- scripts/run_experiments.jl
+++ scripts/run_experiments.jl
@@ -405,4 +405,34 @@ submit_jobs("pre", instance_pattern, "FCS", epsilon_values, "Pre5_fin_sparse_no_
 submit_jobs("pre", instance_pattern, "FCS", epsilon_values, "Pre5_fin_sparse_pre_2", binary_type, 20, config_default)
 
 
+# Mixing test
+
+using PPMP
+include("scripts/run_experiments.jl")
+
+# Configuration: No presolve, No merge, max time
+config_base = ["--is-presolve-flowcut", "false", 
+               "--merge-identical-scenarios", "false",
+               "--max-time", "3600"]
+
+# Test 0: No mixing (baseline)
+config_no_mixing = vcat(config_base, ["--user-mixing-cuts-enabled", "false"])
+submit_jobs("dev6", ["test*"], "FCS", [0.0, 0.01, 0.05, 0.1, 0.2], 
+           "Mix_FCS_no_mixing_dev6", "bin_new2", 1, config_no_mixing)
+
+# Test 1: Basic Star mixing
+config_basic_star = vcat(config_base, ["--mixing-inequality-type", "basic_star"])
+submit_jobs("dev6", ["test*"], "FCS", [0.0, 0.01, 0.05, 0.1, 0.2], 
+           "Mix_FCS_basic_star_dev6", "bin_new2", 1, config_basic_star)
+
+# Test 2: Complement mixing
+config_complement = vcat(config_base, ["--mixing-inequality-type", "complement"])
+submit_jobs("dev6", ["test*"], "FCS", [0.0, 0.01, 0.05, 0.1, 0.2], 
+           "Mix_FCS_complement_dev6", "bin_new2", 1, config_complement)
+
+# Test 3: Both mixing types
+config_both = vcat(config_base, ["--mixing-inequality-type", "both"])
+submit_jobs("dev6", ["test*"], "FCS", [0.0, 0.01, 0.05, 0.1, 0.2], 
+           "Mix_FCS_both_dev6", "bin_new2", 1, config_both)
+
 """
